8be9687f8bb1c893dfec048137d0be87b7b869ef
Axonarage <87577114+Axonarage@users.noreply.github.com>
2024-11-06 16:21:27 +0100

    Populates Exceptions vectors in vector table + setting up VTOR

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -7,18 +7,90 @@
 //!   - the **initial Stack Pointer**
 //!   - the **Reset vector** (pointer to the Reset handler)
 //! 
-//! This crates takes care of RAM initialization, and will populates the vector table.
+//! This crates takes care of RAM initialization, and populates the vector table like this :
+//! 
+//! ```
+//! Vector Table
+//! +------------------+ 0x0
+//! | Initial SP       |
+//! +------------------+ 0x4
+//! | (1) Reset        |
+//! +------------------+ 0x8
+//! | (2) NMI          |
+//! +------------------+ 0xc
+//! | (3) HardFault    |
+//! +------------------+ 0x10
+//! | (4) MemManage    |
+//! +------------------+ 0x14
+//! | (5) BusFault     |
+//! +------------------+ 0x18
+//! | (6) UsageFault   |
+//! +------------------+ 0x1c
+//! | (7-10) Reserved  |
+//! +------------------+ 0x2c
+//! | (11) SVCall      |
+//! +------------------+ 0x30
+//! | (12-13) Reserved |
+//! +------------------+ 0x38
+//! | (14) PendSV      |
+//! +------------------+ 0x3c
+//! | (15) SysTick     |
+//! +------------------+ 0x40
+//! | IRQ_n            |
+//! | ...              |
+//! ```
+
 
 use core::ptr;
 use core::sync::atomic::{compiler_fence, Ordering};
 
 mod panic;
+mod handlers;
 use crate::main;
 
-/// Here is the Reset vector, a pointer to our Reset handler : Reset()
+#[repr(C)]
+#[allow(non_snake_case)]
+pub struct ExceptionsHandlers {
+    NMI: unsafe extern "C" fn() -> !,
+    HardFault: unsafe extern "C" fn() -> !,
+    MemManage: unsafe extern "C" fn() -> !,
+    BusFault: unsafe extern "C" fn() -> !,
+    UsageFault: unsafe extern "C" fn() -> !,
+    Reserved_7: u32,
+    Reserved_8: u32,
+    Reserved_9: u32,
+    Reserved_10: u32,
+    SVCall: unsafe extern "C" fn() -> !,
+    Reserved_12: u32,
+    Reserved_13: u32,
+    PendSV: unsafe extern "C" fn() -> !,
+    SysTick: unsafe extern "C" fn() -> !
+}
+
+/// Reset vector, part of the Vector table, points to our Reset handler : Reset()
 #[unsafe(link_section = ".vector_table.reset_vector")]
 #[unsafe(no_mangle)]
 pub static RESET_VECTOR: unsafe extern "C" fn() -> ! = Reset;
+
+/// Exceptions vectors, part of the Vector table
+#[unsafe(link_section = ".vector_table.exceptions")]
+#[unsafe(no_mangle)]
+pub static _EXCEPTIONS: ExceptionsHandlers = ExceptionsHandlers {
+    NMI: handlers::DefaultHandler,
+    HardFault: handlers::DefaultHandler,
+    MemManage: handlers::DefaultHandler,
+    BusFault: handlers::DefaultHandler,
+    UsageFault: handlers::DefaultHandler,
+    Reserved_7: 0,
+    Reserved_8: 0,
+    Reserved_9: 0,
+    Reserved_10: 0,
+    SVCall: handlers::DefaultHandler,
+    Reserved_12: 0,
+    Reserved_13: 0,
+    PendSV: handlers::DefaultHandler,
+    SysTick: handlers::DefaultHandler
+};
 
 
 /// Initialization of the .bss section by zeroing  out memory
@@ -43,6 +115,8 @@ pub unsafe fn init_data(start_data: *mut u8, sidata: *const u8, count: usize){
 pub extern "C" fn Reset() -> ! {
     
     unsafe extern "C" {
+        static _vector_table: u32;
+
         static mut _sbss: u8;
         static mut _ebss: u8;
 
@@ -50,7 +124,13 @@ pub extern "C" fn Reset() -> ! {
         static mut _edata: u8;
         static _sidata: u8;
     }
-    
+
+    // Setting VTOR
+    const VTOR: *mut u32 = 0xe000ed08 as *mut u32;
+    unsafe {
+        ptr::write_volatile(VTOR, ptr::addr_of!(_vector_table) as u32);
+    }
+
     // RAM initialization
     let mut start: *mut u8 = &raw mut _sbss;
     let mut end: *mut u8 = &raw mut _ebss;
