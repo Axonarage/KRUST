1d79cf464fe1b3bc721bdfef97c58b0ec2ceabf5
Axonarage <87577114+Axonarage@users.noreply.github.com>
2024-11-24 20:41:16 +0100

    Add custom test runner

--- src/krust.rs
+++ src/krust.rs
@@ -3,78 +3,31 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(test::test_runner)]
+#![reexport_test_harness_main = "test_runner"]
+
 mod init;
 mod utils;
-use core::arch::asm;
+mod test;
 
 /// Krust main function called by the Reset handler
 pub fn main() -> ! {
     
-    unsafe {
-        asm!(
-            "LDR r0, =0xdeadc0de"
-        );
-    }
     log_debug!("KRUST");
 
     unsafe {
         enable_system_handler_fault();
     }
     
-    init::start_sys_tick();
-    
-    /*
-    // Trigger HardFaultHandler with Undefined instruction usage fault.
-    unsafe {
-        asm!("udf #0");
-    }
-    */
-    /*
-    unsafe {
-        // Trigger INVPC by manually loading an invalid value into the PC
-        asm!("LDR R0, =0xFFFFFFFF"); // Load an invalid address
-        asm!("BX R0"); // Branch to the invalid address
+    #[cfg(test)]
+    test_runner();
 
-        log_debug!("This line will not be reached due to INVPC fault.");
-    }
-    */
-    // unsafe {
-    //     // Attempt to access an unimplemented coprocessor
-    //     trigger_lsperr();
-
-    log_debug!("KRUST");
+    init::start_sys_tick();   
 
     loop {}
 }
 
-
-#[inline(never)]
-unsafe fn trigger_nmi() {
-    const NVIC_ICSR: *mut u32 = 0xE000ED04 as *mut u32; // Address of ICSR
-    unsafe {
-        core::ptr::write_volatile(NVIC_ICSR, 1 << 31); // Set NMIPENDSET bit
-    }
-}
-
-
-#[inline(never)]
-unsafe fn trigger_nocp() {
-    unsafe {
-        asm!(
-            "MRC p15, 0, r0, c15, c0, 0", // Access a system control register via coprocessor
-            options(nostack)
-        );
-    }
-}
-
-#[inline(never)]
-unsafe fn trigger_lsperr() {
-    let _ = 1.0f32 + 2.0f32; // Perform floating-point operation
-    unsafe {
-        asm!("LDR R0, =0xFFFFFFFF"); // Load an invalid address
-        asm!("LDR R1, [R0]");        // Access invalid memory
-    }
-}
 
 unsafe fn enable_system_handler_fault() {
     unsafe {
---
+++ src/test/mod.rs
@@ -0,0 +1,12 @@
+use crate::log_debug;
+
+//mod exception_test;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+
+    for (i,test) in tests.into_iter().enumerate() {
+        log_debug!("Run test {}",i);
+        test();
+    }
+}
